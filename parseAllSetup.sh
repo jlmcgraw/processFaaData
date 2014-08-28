c9b56d75588f25938ec5ca086948bb6dc2b79d2a
Jesse McGraw <jlmcgraw@gmail.com>
2014-08-28 09:01:07 -0400

    Updates

--- parseAllSetup.sh
+++ parseAllSetup.sh
@@ -4,26 +4,31 @@ IFS="`printf '\n\t'`"  # Always put this in Bourne shell scripts
 
 
 #Input and output directories
-56dayCurrent = 
-56dayUrl = https://nfdc.faa.gov/webContent/56DaySub/56DySubscription_July_24__2014_-_September_18__2014.zip
+#56dayCurrent = 
+#56dayUrl = https://nfdc.faa.gov/webContent/56DaySub/56DySubscription_July_24__2014_-_September_18__2014.zip
 
-datadir=./56DySubscription_May_29__2014_-_July_24__2014/
+datadir=./56DySubscription_July_24__2014_-_September_18__2014/
 outputdir=./outputData
 
-#Clear out old CSV files
+
+
+#get current datafile
+#unzip to datadir
+
+#get obstacle file
+#unzip to datadir
+#remove the header lines from obstacle file
+
 set +e
-rm $outputdir/*.csv
+rm ./56day.db
 set -e
 
-get current datafile
-unzip to datadir
+./parseAll.pl -g -e $datadir
 
-get obstacle file
-unzip to datadir
-remove the header lines from obstacle file
+cp 56day.db spatial56day.db
 
-./parseAll.pl
+#convert to spatialite
+sqlite3 spatial56day.db < sqliteToSpatialite.sql
 
-add indexes
+#add indexes
 
-convert to spatialite
--- processFaaData.pm
+++ processFaaData.pm
@@ -27,7 +27,8 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 
 $VERSION = 1.00;
 @ISA     = qw(Exporter);
-@EXPORT  = qw(trim rtrim ltrim coordinatetodecimal coordinatetodecimal2 is_vhf);
+@EXPORT =
+  qw(trim rtrim ltrim coordinatetodecimal coordinatetodecimal2 is_vhf coordinateToDecimalCifpFormat);
 
 #@EXPORT_OK   = qw(rtrim ltrim coordinatetodecimal);
 
@@ -39,21 +40,21 @@ my $debug = 0;
 #        s/^\s+//;          # trim left
 #        s/\s+$//;          # trim right
 #    }
-#    return @out == 1 
+#    return @out == 1
 #              ? $out[0]   # only one to return
 #              : @out;     # or many
 #}
 
 sub trim {
 
-# 1. trim leading and trailing white space
-# 2. collapse internal whitespace to single space each
-# 3. take input from $_ if no arguments given
-# 4. join return list into single scalar with intervening spaces 
-#     if return is scalar context
+    # 1. trim leading and trailing white space
+    # 2. collapse internal whitespace to single space each
+    # 3. take input from $_ if no arguments given
+    # 4. join return list into single scalar with intervening spaces
+    #     if return is scalar context
 
     my @out = @_ ? @_ : $_;
-    $_ = join(' ', split(' ')) for @out;
+    $_ = join( ' ', split(' ') ) for @out;
     return wantarray ? @out : "@out";
 }
 
@@ -74,6 +75,7 @@ sub ltrim($) {
 sub is_vhf($) {
     my $freq = shift;
     if ( $freq =~ m/(1[1-3][0-9]\.\d{1,3})/ && ( $1 >= 118 && $1 < 137 ) ) {
+
         # print "$freq is VHF";
         return 1;
     }
@@ -85,13 +87,10 @@ sub is_vhf($) {
 }
 
 sub coordinatetodecimal {
+
     # my ($coordinate) = @_;
-        #Validate and set input parameters to this function
-   my ( $coordinate ) =
-      validate_pos(
-        @_,
-        { type => SCALAR }
-      );
+    #Validate and set input parameters to this function
+    my ($coordinate) = validate_pos( @_, { type => SCALAR } );
     my ( $deg, $min, $sec, $signeddegrees, $declination );
 
     #Remove any whitespace
@@ -102,6 +101,7 @@ sub coordinatetodecimal {
     return "0" if !( $declination =~ /[NSEW]/ );
 
     $coordinate =~ m/^(\d{1,3})-/;
+
     #print $1;
     $deg = $1 / 1;
     $coordinate =~ m/-(\d{2})-/;
@@ -142,26 +142,26 @@ sub coordinatetodecimal {
 sub coordinatetodecimal2 {
 
     #Validate and set input parameters to this function
-   my ( $deg, $min, $sec,  $declination ) =
-      validate_pos(
+    my ( $deg, $min, $sec, $declination ) = validate_pos(
         @_,
         { type => SCALAR },
         { type => SCALAR },
         { type => SCALAR },
         { type => SCALAR },
 
-      );
+    );
 
-    # my ( $deg, $min, $sec,  $declination ) = @_;
     my $signedDegrees;
-    
-    return "0" if !( $declination =~ /[NSEW]/ );
 
-    
+    if ( ! $declination =~ /[NSEW]/ ) {
+        say "Bad declination parameter: $declination";
+        return 0; 
+        }
+
     $deg = $deg / 1;
-    
+
     $min = $min / 60;
-    
+
     $sec = $sec / 3600;
 
     $signedDegrees = ( $deg + $min + $sec );
@@ -170,21 +170,112 @@ sub coordinatetodecimal2 {
         when (/S|W/) {
             $signedDegrees = -($signedDegrees);
         }
-        
+
         when (/N|S/) {
 
             #Latitude is invalid if less than -90  or greater than 90
-            $signedDegrees = "0" if ( abs($signedDegrees) > 90 );
+            if ( abs($signedDegrees) > 90 ){
+                say "$signedDegrees is out of valid range for latitude";
+                $signedDegrees = 0; }
         }
         when (/E|W/) {
 
             #Longitude is invalid if less than -180 or greater than 180
-            $signedDegrees = "0" if ( abs($signedDegrees) > 180 );
+            if ( abs($signedDegrees) > 180 ){
+            say "$signedDegrees is out of valid range for longitude";
+           $signedDegrees = 0  ;
+           }
         }
         default {
         }
 
     }
+    return ($signedDegrees);
+}
+
+sub coordinateToDecimalCifpFormat {
+
+    #Convert a latitude or longitude in CIFP format to its decimal equivalent
+    my ($coordinate) = shift;
+    my ( $deg, $min, $sec, $signedDegrees, $declination, $secPostDecimal );
+    my $data;
+
+    #First parse the common information for a record to determine which more specific parser to use
+    my $parser_latitude = Parse::FixedLength->new(
+        [
+            qw(
+              Declination:1
+              Degrees:2
+              Minutes:2
+              Seconds:2
+              SecondsPostDecimal:2
+              )
+        ]
+    );
+    my $parser_longitude = Parse::FixedLength->new(
+        [
+            qw(
+              Declination:1
+              Degrees:3
+              Minutes:2
+              Seconds:2
+              SecondsPostDecimal:2
+              )
+        ]
+    );
+
+    #Get the first character of the coordinate and parse accordingly
+    $declination = substr( $coordinate, 0, 1 );
+
+    given ($declination) {
+        when (/[NS]/) {
+            $data = $parser_latitude->parse_newref($coordinate);
+            die "Bad input length on parser_latitude"
+              if ( $parser_latitude->length != 9 );
+
+            #Latitude is invalid if less than -90  or greater than 90
+            # $signedDegrees = "" if ( abs($signedDegrees) > 90 );
+        }
+        when (/[EW]/) {
+            $data = $parser_longitude->parse_newref($coordinate);
+            die "Bad input length on parser_longitude"
+              if ( $parser_longitude->length != 10 );
+
+            #Longitude is invalid if less than -180 or greater than 180
+            # $signedDegrees = "" if ( abs($signedDegrees) > 180 );
+        }
+        default {
+            return -1;
+
+        }
+    }
+
+    $declination    = $data->{Declination};
+    $deg            = $data->{Degrees};
+    $min            = $data->{Minutes};
+    $sec            = $data->{Seconds};
+    $secPostDecimal = $data->{SecondsPostDecimal};
+
+    # print Dumper($data);
+
+    $deg = $deg / 1;
+    $min = $min / 60;
+
+    #Concat the two portions of the seconds field with a decimal between
+    $sec = ( $sec . "." . $secPostDecimal );
+
+    # say "Sec: $sec";
+    $sec           = ($sec) / 3600;
+    $signedDegrees = ( $deg + $min + $sec );
+
+    #Make coordinate negative if necessary
+    if ( ( $declination eq "S" ) || ( $declination eq "W" ) ) {
+        $signedDegrees = -($signedDegrees);
+    }
+
+    # say "Coordinate: $coordinate to $signedDegrees";           #if $debug;
+    # say "Decl:$declination Deg: $deg, Min:$min, Sec:$sec";    #if $debug;
+
     return ($signedDegrees);
 }
 1;
