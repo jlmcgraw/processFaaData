671270aec51d45a4006e6f5186e222c1937113c3
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-06-02 11:13:55 -0400

    Update Setup

--- setup.sh
+++ setup.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
-set -eu                # Always put this in Bourne shell scripts
-IFS=$(printf '\n\t')   # Always put this in Bourne shell scripts
 
 #Install necessary software
 apt install -y \
                     git \
                     sqlite3 \
-                    perltidy \
-                    cpanminus \
-                    carton \
                     libspatialite-dev \
                     spatialite-bin \
                     libsqlite3-mod-spatialite \
                     gdal-bin \
                     unzip
 
-#Install the libraries in our cpanfile locally
-carton install
-
-#Setup hooks to run perltidy on git commit
-cat > .git/hooks/pre-commit << 'EOF'
-#!/bin/bash
-find . -maxdepth 1 -type f -name '*.pl' -or -name '*.pm' | \
-    xargs -I{} -P0 sh -c 'perltidy -b -noll {}'
-EOF
-
-chmod +x .git/hooks/pre-commit
+#Install necessary libraries
+pip install requests
